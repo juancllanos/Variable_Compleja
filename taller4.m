823f336c560a284e149b7b2e29831861fb53373c
juancllanos <33555510+juancllanos@users.noreply.github.com>
2019-09-04 12:38:39 -0500

    Add files via upload

--- taller4.m
+++ taller4.m
@@ -1,40 +1,47 @@
-X = [];
-Y = [];
-Z = [];
+% r = 1;
+% xo = 0;
+% yo = 0;
+% zo = 0;
+% m =20;
+% n = 10;
+% theta=0:2*pi/m:2*pi ;
+% phi=-pi/2:pi/10:pi/2 ;
+% [T,P] = meshgrid(theta,phi) ;
+% X1 = xo + r *cos(P).* cos(T);
+% Y1 = yo + r *cos(P).* sin(T);
+% Z1 = zo + r *sin(P) ;
+% 
+% plot3(X1,Y1,Z1);
+% hold on
 
-R = [];
-I = [];
+x0 = 0;
+y0 = 0;
+r = 1;
 
-n = 1;
-for i=-2:0.5:2
-    a = complex(i,2);
-    [xa,ya,za]  = comp2esf(a);
-    R(n) = real(a);
-    I(n) = imag(a);
-    X(n) = xa;
-    Y(n) = ya;
-    Z(n) = za;
-    n = n+1;
+% [X,Y] = circ(x0,y0,r);
+
+[X1,Y1] = rombo(x0,y0,r);
+plot(X1,Y1);
+hold on 
+[X2,Y2] = circ(x0,y0,r);
+plot(X2,Y2);
+
+for i=1:0.4:3
+  X = [X X*i NaN];
+  Y = [Y Y*i NaN];
+  
+  plot(X,Y);
+    
 end
 
-plot(R,I);
+% C = complex(X,Y);
+% 
+% [Xe,Ye,Ze] = comp2esf(C);
+% disp(Xe);
+% disp(Ye);
+% disp(Ze);
+% plot3(Xe,Ye,Ze);
 
-r = 1;
-xo = 0;
-yo = 0;
-zo = 0;
-m =20;
-n = 10;
-theta=0:2*pi/m:2*pi ;
-phi=-pi/2:pi/10:pi/2 ;
-[T,P] = meshgrid(theta,phi) ;
-X1 = xo + r *cos(P).* cos(T);
-Y1 = yo + r *cos(P).* sin(T);
-Z1 = zo + r *sin(P) ;
-
-plot3(X1,Y1,Z1);
-hold on
-plot3(X,Y,Z);
 
 
 
@@ -44,7 +51,15 @@ Y = (2*imag(z))/(norm(z)^2 + 1);
 Z = ((norm(z)^2 - 1))/(norm(z)^2 + 1);
 end
 
-function [X,Y] = esf2comp(x,y,z)
-X = x/(1-z);
-Y = y/(1-z);
+function [X,Y] = circ(x0,y0,r)
+X = linspace(x0-r,x0+r,20);
+Y = sqrt(r^2-X.^2);
+X = [X fliplr(X)];
+Y = [Y -fliplr(Y)] + y0;
 end
+
+function [X,Y] = rombo(x0,y0,c)
+X = [x0-c,x0,x0+c,x0,x0-c];
+Y = [y0,y0-c,y0,y0+c,y0];
+end
+
